8a151de4d348fd705b6d9006270710df20600372
Abhijit Menon-Sen <ams@toroid.org>
2001-05-13 14:23:17 +0000

    Import of AMS/Crypt-GOST-1.00 from CPAN.
    gitpan-cpan-distribution: Crypt-GOST
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         AMS/Crypt-GOST-1.00.tar.gz
    gitpan-cpan-author:       AMS
    gitpan-cpan-maturity:     released

--- test.pl
+++ test.pl
@@ -1,20 +1,90 @@
-# Before `make install' is performed this script should be runnable with
-# `make test'. After `make install' it should work as `perl test.pl'
+use strict;
 
-######################### We start with some black magic to print on failure.
+use Test;
+use vars qw($loaded);
+use Benchmark qw(timediff timestr);
 
-# Change 1..1 below to 1..last_test_to_print .
-# (It may become useful if the test is moved to ./t subdirectory.)
+BEGIN { plan tests => 6 }
+END   { print "not ok 1\n" unless $loaded }
 
-BEGIN { $| = 1; print "1..1\n"; }
-END {print "not ok 1\n" unless $loaded;}
+my $key       = pack "H*", '1234567890ABCDEFFEDCBA0987654321'x2;
+my $plaintext = "The quick brown fox jumps over the lazy dog.";
+
 use Crypt::GOST;
-$loaded = 1;
-print "ok 1\n";
+ok($loaded = 1);
+ok(my $gost = Crypt::GOST->new("abcdefghijklmnop"x2));
+ok("aaaabbbb", $gost->decrypt($gost->encrypt("aaaabbbb")));
+ok(my $old = Crypt::GOST->new);
+$old->generate_sbox($key); $old->generate_keys($key);
+ok(
+    # The old SSdecrypt used to return spurious padding material.
+    substr($old->SSdecrypt($old->SScrypt($plaintext)), 0, length $plaintext),
+    $plaintext
+);
 
-######################### End of black magic.
+eval 'use Crypt::CBC';
+if ($@) { print "skipping Crypt::CBC test\n"; }
+else {
+    print "trying CBC... ";
+    my $c = Crypt::CBC->new($key, "GOST") || die "$!\n";
+    my $t = $c->encrypt_hex($plaintext);
+    ok($plaintext, $c->decrypt_hex($t));
+}
 
-# Insert your test code below (better if it prints "ok 13"
-# (correspondingly "not ok 13") depending on the success of chunk 13
-# of the test code):
+print "\nBenchmarks\n";
+{
+    my $s = Benchmark->new;
 
+    for (my $i = 0; $i < 10000; $i++) {
+        my $c  = Crypt::GOST->new($key);
+        my $in = pack "H*", "1234567890ABCDEF";
+        $c->encrypt($in);
+    }
+
+    my $t = Benchmark->new;
+
+    print "Encrypting (10,000 cycles, uncached cipher): ",
+          timestr(timediff($t, $s)), "\n";
+}
+{
+    my $s = Benchmark->new;
+
+    for (my $i = 0; $i < 10000; $i++) {
+        my $c  = Crypt::GOST->new($key);
+        my $in = pack "H*", "1234567890ABCDEF";
+        $c->decrypt($in);
+    }
+
+    my $t = Benchmark->new;
+
+    print "Decrypting (10,000 cycles, uncached cipher): ",
+          timestr(timediff($t, $s)), "\n";
+}
+{
+    my $c = Crypt::GOST->new($key);
+    my $s = Benchmark->new;
+
+    for (my $i = 0; $i < 10000; $i++) {
+        my $in = pack "H*", "1234567890ABCDEF";
+        $c->encrypt($in);
+    }
+
+    my $t = Benchmark->new;
+
+    print "Encrypting (10,000 cycles,   cached cipher): ",
+          timestr(timediff($t, $s)), "\n";
+}
+{
+    my $c = Crypt::GOST->new($key);
+    my $s = Benchmark->new;
+
+    for (my $i = 0; $i < 10000; $i++) {
+        my $in = pack "H*", "1234567890ABCDEF";
+        $c->decrypt($in);
+    }
+
+    my $t = Benchmark->new;
+
+    print "Decrypting (10,000 cycles,   cached cipher): ",
+          timestr(timediff($t, $s)), "\n";
+}
